1e64582c1f6d48765304e65cdecaf3113a13439b
songdanyang <18361223628@163.com>
2019-11-20 10:50:02 +0800

    add

--- pkg/apis/rabbitmq/v1alpha1/doc.go
+++ pkg/apis/rabbitmq/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the rabbitmq v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=rabbitmq.xes.com
+// +groupName=rabbitmq.cloud.com
 package v1alpha1
--- pkg/apis/rabbitmq/v1alpha1/register.go
+++ pkg/apis/rabbitmq/v1alpha1/register.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "rabbitmq.xes.com", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "rabbitmq.cloud.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
